93fbeb56201fc64056750041b740f2f582e3eac6
ChunBaee <mljung10@naver.com>
2023-04-08 15:03:58 +0900

    init: 프로젝트 생성

---
+++ app/src/main/java/com/chunbae/narchive/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chunbae.narchive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
