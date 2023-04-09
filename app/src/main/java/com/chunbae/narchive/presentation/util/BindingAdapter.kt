36accfce6737107263a1da7355a54eb922feb655
ChunBaee <mljung10@naver.com>
2023-04-09 20:07:52 +0900

    #4 ui: 피드 일반 일기 아이템 레이아웃 작성

--- app/src/main/java/com/chunbae/narchive/presentation/util/BindingAdapter.kt
+++ app/src/main/java/com/chunbae/narchive/presentation/util/BindingAdapter.kt
@@ -1,10 +1,18 @@
 package com.chunbae.narchive.presentation.util
 
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 @BindingAdapter("Common_Circle_Image")
 fun ImageView.setCircleImage(path : Any) {
     Glide.with(this).load(path).circleCrop().into(this)
+}
+
+@BindingAdapter("Common_iv_radius_5")
+fun ImageView.setRadius5Image(path : Any) {
+    Glide.with(this).load(path).transform(CenterCrop(), RoundedCorners(5)).into(this)
 }
