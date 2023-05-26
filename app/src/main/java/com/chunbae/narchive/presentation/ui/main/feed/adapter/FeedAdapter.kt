ce47b4d613a0d7f04e8e4dbdcc14e4627146d9c7
ChunBaee <mljung10@naver.com>
2023-05-26 15:58:07 +0900

    #36 fix: 간단일기 피드 Rv에서 Chip으로 수정

--- app/src/main/java/com/chunbae/narchive/presentation/ui/main/feed/adapter/FeedAdapter.kt
+++ app/src/main/java/com/chunbae/narchive/presentation/ui/main/feed/adapter/FeedAdapter.kt
@@ -1,14 +1,19 @@
 package com.chunbae.narchive.presentation.ui.main.feed.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.chunbae.narchive.R
 import com.chunbae.narchive.data.data.FeedData
 import com.chunbae.narchive.databinding.ItemFeedNormalDiaryBinding
 import com.chunbae.narchive.databinding.ItemFeedSimpleDiaryBinding
+import com.google.android.material.chip.Chip
 
 class FeedAdapter (private val onClicked : (Int) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     var feedDatas = mutableListOf<FeedData>()
+    private lateinit var context : Context
 
     inner class NormalDiaryViewHolder(private val binding : ItemFeedNormalDiaryBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item : FeedData) {
@@ -20,13 +25,23 @@ class FeedAdapter (private val onClicked : (Int) -> Unit) : RecyclerView.Adapter
     inner class SimpleDiaryViewHolder(private val binding : ItemFeedSimpleDiaryBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item : FeedData) {
             binding.feedData = item
-            binding.itemFeedSimpleDiaryRvKeywords.adapter = item.keywords?.let { SimpleDiaryKeywordAdapter(it) }
+            item.keywords?.let {
+                for(i in it) {
+                    binding.itemFeedSimpleDiaryRvKeywords.addView(Chip(context).apply {
+                        text = i
+                        isCloseIconVisible = false
+                        chipBackgroundColor = ResourcesCompat.getColorStateList(resources, R.color.color_B2F0F4, null)
+                        setTextColor(ResourcesCompat.getColorStateList(resources, R.color.white, null))
+                    })
+                }
+            }
             binding.root.setOnClickListener { onClicked.invoke(item.feedIdx) }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         //:TODO 추후에 아이템 없을때 ViewHolder도 추가하기
+        context = parent.context
         return when(viewType) {
             1 -> SimpleDiaryViewHolder(ItemFeedSimpleDiaryBinding.inflate(LayoutInflater.from(parent.context), parent, false))
             else -> NormalDiaryViewHolder(ItemFeedNormalDiaryBinding.inflate(LayoutInflater.from(parent.context), parent, false))
